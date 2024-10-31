7a21f7ba2a89b77af6fc4ebfc2fd4b8122239b90
Albert Tian <atian@berkeley.edu>
2024-10-30 19:29:46 -0700

    finishing touches

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,9 +9,21 @@ const queryClient = new QueryClient();
 export default function Home() {
   return (
     <QueryClientProvider client={queryClient}>
+      <div className="bg-gradient-to-r from-red-950 to-red-700 p-4 shadow-md">
+        <div className="max-w-7xl mx-auto flex items-center">
+          <div className="flex items-center">
+            <img
+              src="/slash_logo.svg"
+              alt="Slash Logo"
+              className="h-8 w-auto"
+            />
+          </div>
+        </div>
+      </div>
+
       <div className="min-h-screen bg-gray-100">
         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-          <h1 className="text-3xl font-bold text-gray-900  bg-gray-100 z-1 pt-12 pb-4">
+          <h1 className="text-3xl font-bold text-gray-900 bg-gray-100 z-1 pt-12 pb-4">
             Transactions
           </h1>
           <TransactionList />
